d550e029cd41d2c7f5c314c948af8d0f212831a5
rsshonjoydas <rsshonjoydas@gmail.com>
2022-02-25 16:26:52 +0600

    Routes file

---
+++ client/src/routes/goalRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Set goals" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Update goal id: ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Delete goal id: ${req.params.id}` });
+});
+
+export default router;
