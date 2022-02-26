90af6b8b6e5d0639418f1078121bc1747dc6c9b0
rsshonjoydas <rsshonjoydas@gmail.com>
2022-02-26 11:59:56 +0600

    Delete User Goal

--- client/src/controllers/goalController.js
+++ client/src/controllers/goalController.js
@@ -79,8 +79,19 @@ const deleteGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
 
   if (!goal) {
-    res.status(400);
-    throw new Error("Goal not found");
+    res.status(400).json({ message: "Goal not found" });
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // ? Check for user
+  if (!user) {
+    res.status(401).json({ message: "User not found" });
+  }
+
+  // ? Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401).json({ message: "User not authorized" });
   }
 
   await goal.remove();
