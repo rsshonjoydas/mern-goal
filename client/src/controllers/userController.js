e2cd667b54c77d3301fd0843736c5ea32c79a09f
rsshonjoydas <rsshonjoydas@gmail.com>
2022-02-26 09:32:51 +0600

    Register User

--- client/src/controllers/userController.js
+++ client/src/controllers/userController.js
@@ -1,4 +1,6 @@
+import bcrypt from "bcryptjs";
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel";
 
 /**
  * @param  {} req
@@ -8,7 +10,39 @@ import asyncHandler from "express-async-handler";
  * @access public
  */
 const register = asyncHandler(async (req, res) => {
-  res.json({ message: "register user" });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400).json({ message: "Please add all required fields" });
+  }
+
+  // ? check if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400).json({ message: "User already exists" });
+  }
+
+  // ? Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // ? Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400).json({ message: "Invalid user data" });
+  }
 });
 
 /**
