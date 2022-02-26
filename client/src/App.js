a4570d23103b7f5f9c460a2a6004489273b75aee
rsshonjoydas <rsshonjoydas@gmail.com>
2022-02-26 17:31:17 +0600

    Header & Navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
+
 const App = () => {
   return (
     <>
       <Router>
         <div className="container">
+          <Header />
           <Routes>
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
