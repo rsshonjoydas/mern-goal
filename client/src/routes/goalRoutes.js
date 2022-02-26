89dd29ffee195e45f29f5469b1b4cd349c9945f1
rsshonjoydas <rsshonjoydas@gmail.com>
2022-02-26 11:40:29 +0600

    Protect Goal Routes

--- client/src/routes/goalRoutes.js
+++ client/src/routes/goalRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import goalController from "../controllers/goalController";
+import protect from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.route("").get(goalController.getGoals).post(goalController.postGoal);
-router.route("/:id").put(goalController.updateGoal).delete(goalController.deleteGoal);
+router.route("").get(protect, goalController.getGoals).post(protect, goalController.postGoal);
+router
+  .route("/:id")
+  .put(protect, goalController.updateGoal)
+  .delete(protect, goalController.deleteGoal);
 
 export default router;
