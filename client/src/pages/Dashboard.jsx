afe0ab08496ef988aa7b768cb08fbf98f3a4da8d
rsshonjoydas <rsshonjoydas@gmail.com>
2022-03-05 22:00:44 +0600

    Restrict the Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react"
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 const Dashboard = () => {
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login')
+    }
+  }, [user, navigate])
+
   return (
     <div>Dashboard</div>
   )
